5bf32911a797925eb866345f5cb5efb670ce3bb6
Andrew Diaz <andrewdiazn20@gmail.com>
2024-04-05 13:43:38 -0700

    last commit

--- main.py
+++ main.py
@@ -54,26 +54,37 @@ print(clean(data))
 #<------------------------------------------------>#
 # 2 Object Oriented Prompt
 # Watto needs a new system for organizing his inventory of podracers. Help him do this by implementing an Object Oriented solution according to the following criteria.
+# First, he'll need a general Podracer class defined with max_speed, condition (perfect, trashed, repaired) and price attributes.
+# Define a repair() method that will update the condition of the podracer to "repaired".
+# Define a new class, AnakinsPod that inherits the Podracer class, but also contains a special method called boost that will multiply max_speed by 2.
+# Define another class that inherits Podracer and call this one SebulbasPod. This class should have a special method called flame_jet that will update the condition of another podracer to "trashed".
+
 
 class Podracer:
   def __init__(self,max_speed,condition,price):
     self.max_speed = max_speed
-    self.condtion = condition
+    self.condition = condition
     self.price = price
 
 
-  # def repair:
-  # if('condition' == 'trashed'):
-  #   return 'repaired'
+  def repair(self):
+    self.condition = "repaired"
 
-# class AnakinPod(Podracer):
-#   def boost(max_speed):
-#     return max_speed*2
-
-
-# class SebulbasPod:
-#   def __init__(self, ):
-#     self.con
+class AnakinPod(Podracer):
+  def __init__(self, max_speed,condition,price):
+    super.__init__(max_speed,condition,price)
 
 
-print(Podracer(max_speed='100',condition='trashed', price='$100'))
+    def boost(self):
+      self.max_speed *= 2
+
+
+class SebulbasPod(Podracer):
+  def __init__(self, max_speed,condition,price):
+    super.__init__(max_speed, condition, price)
+
+    def flame_jet(self,other):
+      other.condition = "trashed"
+
+
+
